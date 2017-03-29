e4a87277a85cf705fb1f0eaadb78446a8937f0ce
Md. Shadman Hasan <Md. Shadman Hasan>
2017-03-29 17:00:27 +0600

    Edited Just To See How Much Changes Are Showed

--- Test.java
+++ Test.java
@@ -31,24 +31,5 @@ public class Test {
 		}	else {
 			System.out.println("Invalid Input");
 		}
-
-		
-		//System.out.print("Please Provide Your Age: ");
-
-		//int age = getUserInput.nextInt();
-
-		//double number1 = getUserInput.nextDouble();
-		//double number2 = getUserInput.nextDouble();
-
-		//System.out.println("You Have Given " + number1 + " and " + number2 + " And their sum is: " + (number1 + number2));
-
-		
-		//System.out.println("You Are " + age + " Years Old");
-
-		/*System.out.println("The Followinng is My Info");
-		System.out.println("Name: Md. Shadman Hasan");
-		System.out.println("Dept.: Information and Communication Engineering");
-		System.out.println("Roll No.: 14065642");
-		System.out.println("Reg. No.: 14HRH1143");*/
-		}	
+	}	
 }
